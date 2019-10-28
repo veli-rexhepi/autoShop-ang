63d1f1730686957173342e1b1c14ba1747801ddc
veli_rexhepi@yahoo.com <veli_rexhepi@yahoo.com>
2019-10-28 14:24:07 +0300

    Commit 1 / 28-Oct-19

---
+++ src/app/classes/car.ts
@@ -0,0 +1,14 @@
+export class Car {
+
+  constructor(
+    public id: number,
+    public imageFile: string,
+    public title: string,
+    public enginePower: string,
+    public engineSize: string,
+    public fuelType: string,
+    public transmitionType: string,
+    public extraInfo: string,
+    public price: string
+  ) { }
+}
